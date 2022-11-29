43da6be795c4bb97ccd1267004c4b8921e08d7f6
Jhonny Moreira <jhonnymoreira@protonmail.com>
2022-11-29 07:04:19 -0300

    feat: enhance eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,6 +8,10 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:astro/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
   ],
   overrides: [
     {
@@ -24,7 +28,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react', 'jsx-a11y'],
   rules: {
     'sort-imports': [
       'error',
