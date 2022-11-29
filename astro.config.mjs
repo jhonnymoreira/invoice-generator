0f2f91a40a57426d7849c8cb3ff6f7194d08a332
Jhonny Moreira <jhonnymoreira@protonmail.com>
2022-11-29 07:04:52 -0300

    feat: add compressor to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import tailwind from '@astrojs/tailwind';
+import compress from 'astro-compress';
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react(), compress()],
   output: 'static',
 });
